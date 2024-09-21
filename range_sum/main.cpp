b6272ac6ef662eca84ceae5f7610f2e2ff42a7f2
nampluskr <nampluskr@gmail.com>
2024-09-22 07:38:56 +0900

    create

--- range_sum/main.cpp
+++ range_sum/main.cpp
@@ -1,4 +1,9 @@
 #include <cstdio>
+#include "sort2.h"
+
+void testSort(void (*sortFunc)(int[], int, int), int arr[], int low, int high) {
+    sortFunc(arr, low, high);
+}
 
 int main()
 {
---
+++ range_sum/sort2.cpp
@@ -0,0 +1,9 @@
+#include "sort2.h"
+
+void bubbleSort(int arr[], int low, int high) {}
+void insertionSort(int arr[], int low, int high) {}
+void selectionSort(int arr[], int low, int high) {}
+void sellSort(int arr[], int low, int high) {}
+void quickSort(int arr[], int low, int high) {}
+void mergeSort(int arr[], int low, int high) {}
+void heapSort(int arr[], int low, int high) {}
