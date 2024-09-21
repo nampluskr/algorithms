36ccc3fda6154619413934223ac0a4579c809b7c
nampluskr <nampluskr@gmail.com>
2024-09-22 08:00:12 +0900

    update

--- range_sum/main.cpp
+++ range_sum/main.cpp
@@ -1,5 +1,32 @@
 #include <cstdio>
+#include <ctime>
 #include "sort2.h"
+
+#define MAX_SIZE    20          // 배열의 크기
+
+int arr[MAX_SIZE];
+int n = MAX_SIZE;                // 배열의 원소 개수
+int maxNumber = 100;             // 배열의 원소 중 최대값
+unsigned long long seed = 42;
+
+int rand(unsigned long long seed) {
+    seed = seed * 25214903917ULL + 11ULL;
+    return (seed >> 16) & 0x3fffffff;
+}
+
+void initArray(int arr[], int n, int maxNumber, int seed) {
+    for (int i = 0; i < n; i++) {
+        seed = rand(seed);
+        arr[i] = seed % maxNumber;
+    }
+}
+
+void printArray(int arr[], int low, int high) {
+    printf(">> ");
+    for (int i = low; i < high; i++)
+        printf("%d, ", arr[i]);
+    printf("%d\n", arr[high]);
+}
 
 void testSort(void (*sortFunc)(int[], int, int), int arr[], int low, int high) {
     sortFunc(arr, low, high);
@@ -7,6 +34,11 @@ void testSort(void (*sortFunc)(int[], int, int), int arr[], int low, int high) {
 
 int main()
 {
+    int low = 0, high = n - 1;
+    initArray(arr, n, maxNumber, seed);
+    printArray(arr, low, high);
+    testSort(bubbleSort, arr, low, high);
+    printArray(arr, low, high);
 
     return 0;
 }
--- range_sum/sort2.cpp
+++ range_sum/sort2.cpp
@@ -1,6 +1,24 @@
 #include "sort2.h"
 
-void bubbleSort(int arr[], int low, int high) {}
+inline void swap(int& a, int& b) { int temp = a; a = b; b = temp; }
+inline int min(int a, int b) { return (a < b)? a: b; }
+inline int max(int a, int b) { return (a < b)? b: a; }
+
+void bubbleSort(int arr[], int low, int high) {
+    bool isSwapped;
+    for (int i = high; i > low ; i--) {
+        isSwapped = false;
+        for (int j = low; j < i; j++) {
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
+                isSwapped = true;
+        }
+        if (!isSwapped) break;
+    }
+}
+
+
+
 void insertionSort(int arr[], int low, int high) {}
 void selectionSort(int arr[], int low, int high) {}
 void sellSort(int arr[], int low, int high) {}
